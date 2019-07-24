0cf1ee015031084d397cad07f8f899b6b2a98a3a
Kyuka Isaac <kyukaisaac@gmail.com>
2019-07-24 18:14:27 +0300

    Splash theme

--- src/components/PosApp.js
+++ src/components/PosApp.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View, StyleSheet, ImageBackground, ActivityIndicator, Text } from 'react-native';
+import { View, StyleSheet } from 'react-native';
 import NetInfo from "@react-native-community/netinfo";
 
 import Toolbar from './Toolbar';
